1fa3dba2ead8d74c5860da829b93663385b9f2b9
BilliNovik <dangercreo2@aol.com>
2022-05-15 14:35:53 +0300

    i'm starting poject

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import 'dotenv/config';
+
+import postRouter from './routes/posts.js'
+
+const app = express();
+
+app.use('/posts', postRouter)
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = process.env.CONNECTION_URL
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message))
